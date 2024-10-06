ebde609a7d53b6a27b8ba049471eb595c5a24644
SyedRaihanuzzaman <142418984+SyedRaihanuzzaman@users.noreply.github.com>
2024-10-06 21:54:30 +0600

    Background Service is done

---
+++ app/src/main/java/com/example/yourname/services/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.yourname.services
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button1).setOnClickListener{
+
+            val intent = Intent(this,MyService::class.java)
+            intent.action = Actions.START.toString()
+            startService(intent)
+
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener{
+
+            val intent = Intent(this,MyService::class.java)
+            intent.action = Actions.STOP.toString()
+            startService(intent)
+
+        }
+
+    }
+}
